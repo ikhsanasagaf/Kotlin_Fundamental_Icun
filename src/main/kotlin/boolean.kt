50f3596be50111111b2162d6002c625743a41738
ikhsanasagaf <ikhsan.asagaf2012@gmail.com>
2020-08-12 15:09:03 +0700

    update

--- src/main/kotlin/boolean.kt
+++ src/main/kotlin/boolean.kt
@@ -1,14 +1,14 @@
 fun main() {
-    val nilai = 61
+    val nilai = 60
     val hasil : String
 
-    hasil = if (nilai > 90) {
+    hasil = if (nilai > 90 && nilai <=100) {
         "A"
-    } else if (nilai > 80) {
+    } else if (nilai > 80 && nilai <=90) {
         "B"
-    } else if (nilai > 70) {
+    } else if (nilai > 70 && nilai <= 80) {
         "C"
-    } else if (nilai > 60) {
+    } else if (nilai > 60 && nilai <= 70) {
         "D"
     } else {
         "E"
